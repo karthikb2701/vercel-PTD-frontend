4d34567cefb8572972c9eb2cecb60d9b4b895259
Karthikb2701@ <karthikb2701@gmail.com>
2025-06-20 23:41:53 +0530

    url update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://vercel-ptd-backend-fzm6.vercel.app/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
